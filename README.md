f5ac5b6b4a894f2066e45161423630b211c7fc7e
blarth <jminocente@gmail.com>
2023-03-20 20:04:27 -0300

    feat: docker-compose

---
+++ README.md
@@ -0,0 +1,7 @@
+## Readme
+### This repository was created to pratice a code challenge.
+### Docker-hub
+The links for the images are: 
+blarth/node-mysql-nginx
+blarth/nginx
+
--- node/index.js
+++ node/index.js
@@ -1,24 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const port = 3000;
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+app.use(express.json());
 
+app.post("/", async (req, res) => {
+  try {
+    const connection = mysql.createConnection(config);
+    connection.connect();
+    // Inserir novo registro com o nome enviado no corpo da requisição
+    const { name } = req.body;
+    const insertSql = `INSERT INTO people(name) values('${name}')`;
+    connection.query(insertSql);
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+    // Obter todos os nomes da tabela "people"
+    const namesResult = await new Promise((resolve, reject) => {
+      connection.query("SELECT name FROM people", (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        }
+        const names = results.map((result) => result.name);
+        resolve(names);
+      });
+    });
+    const resBody = `<h1>Full Cycle Rocks</h1><p> Nomes cadastrados: ${namesResult.join(
+      ", "
+    )}</p>`;
+    connection.end();
+    res.send(resBody);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
